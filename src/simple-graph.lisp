4161826e7b3f154bf78bd3cc3b98f2c44d7fff72
Colin Woodbury <colin@fosskers.ca>
2025-01-06 06:48:59 +0900

    Expose a stream-generalised variant of `to-dot`

--- src/simple-graph.lisp
+++ src/simple-graph.lisp
@@ -2,7 +2,7 @@
   (:use :cl)
   (:export #:graph #:make-graph #:graph-nodes #:graph-edges
            #:add-node #:add-edge
-           #:to-dot)
+           #:to-dot #:to-dot-with-stream)
   (:documentation "A very simple Graph library."))
 
 (in-package :simple-graph)
@@ -14,10 +14,9 @@
 
 (defun add-node (graph node)
   "Mutably add some NODE data to a GRAPH. Avoids adding the same node twice. Yields
-T when a new node was successfully added, NIL otherwise."
-  (cond ((member node (graph-nodes graph) :test #'equal) nil)
-        (t (push node (graph-nodes graph))
-           t)))
+a truthy value when a new node was successfully added, NIL otherwise."
+  (unless (member node (graph-nodes graph) :test #'equal)
+    (push node (graph-nodes graph))))
 
 (defun add-edge (graph from to)
   "Add an edge between two nodes. The edge ids should be `equal' to the
@@ -25,16 +24,20 @@ corresponding node values themselves, but nothing enforces this."
   (let ((pair (cons from to)))
     (push pair (graph-edges graph))))
 
+(defun to-dot-with-stream (graph stream)
+  "Write the GRAPH in dot format to some STREAM."
+  (format stream "graph {~%")
+  (dolist (node (graph-nodes graph))
+    (format stream "  \"~a\";~%" node))
+  (dolist (edge (graph-edges graph))
+    (destructuring-bind (a . b) edge
+      (format stream "  \"~a\" -- \"~a\";~%" a b)))
+  (format stream "}"))
+
 (defun to-dot (graph)
-  "Write the graph to DOT format."
+  "Write the GRAPH to dot format as a string."
   (with-output-to-string (stream)
-    (format stream "graph {~%")
-    (dolist (node (graph-nodes graph))
-      (format stream "  \"~a\";~%" node))
-    (dolist (edge (graph-edges graph))
-      (destructuring-bind (a . b) edge
-        (format stream "  \"~a\" -- \"~a\";~%" a b)))
-    (format stream "}")))
+    (to-dot-with-stream graph stream)))
 
 #++
 (let ((g (make-graph)))
