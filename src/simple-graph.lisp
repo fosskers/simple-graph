6f3a89a8d3eb138ea3a2336864dff7c3d088ed43
Colin Woodbury <colin@fosskers.ca>
2025-01-06 20:41:22 +0900

    Add `subgraph`

--- src/simple-graph.lisp
+++ src/simple-graph.lisp
@@ -1,7 +1,9 @@
 (defpackage simple-graph
   (:use :cl)
   (:export #:graph #:make-graph #:graph-nodes #:graph-edges
-           #:add-node #:add-edge
+           #:add-node! #:add-edge!
+           #:leaf? #:leaves
+           #:subgraph
            #:to-dot #:to-dot-with-stream)
   (:documentation "A very simple Graph library."))
 
@@ -12,7 +14,7 @@
   (nodes (make-hash-table :test #'equal) :type hash-table)
   (edges (make-hash-table :test #'equal) :type hash-table))
 
-(defun add-node (graph node)
+(defun add-node! (graph node)
   "Mutably add some NODE data to a GRAPH. Avoids adding the same node twice. Yields
 a truthy value when a new node was successfully added, NIL otherwise."
   (unless (gethash node (graph-nodes graph))
@@ -20,9 +22,9 @@ a truthy value when a new node was successfully added, NIL otherwise."
 
 #++
 (let ((g (make-graph)))
-  (add-node g "A"))
+  (add-node! g "A"))
 
-(defun add-edge (graph from to)
+(defun add-edge! (graph from to)
   "Add an edge between two nodes. The edge ids should be `equal' to the
 corresponding node values themselves, but nothing enforces this. Edges between
 nodes can be added before the nodes themselves exist. Multiple edges between the
@@ -33,11 +35,11 @@ same nodes can be added."
 
 #++
 (let ((g (make-graph)))
-  (add-node g :a)
-  (add-node g :b)
-  (add-edge g :a :b)
-  (add-edge g :a :c)
-  (add-edge g :c :d)
+  (add-node! g :a)
+  (add-node! g :b)
+  (add-edge! g :a :b)
+  (add-edge! g :a :c)
+  (add-edge! g :c :d)
   g)
 
 (defun leaf? (graph node)
@@ -47,7 +49,7 @@ same nodes can be added."
 
 #++
 (let ((g (make-graph)))
-  (add-node g :a)
+  (add-node! g :a)
   (leaf? g :a))
 
 (defun leaves (graph)
@@ -62,11 +64,38 @@ same nodes can be added."
 
 #++
 (let ((g (make-graph)))
-  (add-node g :a)
-  (add-node g :b)
-  (add-node g :c)
-  (add-edge g :a :c)
+  (add-node! g :a)
+  (add-node! g :b)
+  (add-node! g :c)
+  (add-edge! g :a :c)
   (leaves g))
+
+(defun subgraph (graph from &rest rest)
+  "Form a new subgraph made up of all the nodes and edges reachable from some given
+starting nodes."
+  (let ((sub (make-graph)))
+    (labels ((recurse (next)
+               (dolist (n next)
+                 (when (not (gethash n (graph-nodes sub)))
+                   (add-node! sub n)
+                   (let ((edges (gethash n (graph-edges graph))))
+                     (dolist (edge edges)
+                       (add-edge! sub n edge))
+                     (recurse edges))))))
+      (recurse (remove-if-not (lambda (n) (gethash n (graph-nodes graph)))
+                              (cons from rest))))
+    sub))
+
+#++
+(let ((g (make-graph)))
+  (add-node! g :a)
+  (add-node! g :b)
+  (add-node! g :c)
+  (add-node! g :d)
+  (add-node! g :e)
+  (add-edge! g :a :c)
+  (add-edge! g :d :e)
+  (subgraph g :a))
 
 (defun to-dot-with-stream (graph stream)
   "Write the GRAPH in dot format to some STREAM."
@@ -88,14 +117,18 @@ same nodes can be added."
 
 #++
 (let ((g (make-graph)))
-  (add-node g "A")
-  (add-node g "A")
-  (add-node g "A")
-  (add-node g "B")
-  (add-node g "C")
-  (add-node g "D")
-  (add-edge g "A" "B")
-  (add-edge g "A" "C")
-  (add-edge g "B" "D")
-  (add-edge g "C" "D")
-  (to-dot g))
+  (add-node! g "A")
+  (add-node! g "A")
+  (add-node! g "A")
+  (add-node! g "B")
+  (add-node! g "C")
+  (add-node! g "D")
+  (add-node! g "E")
+  (add-node! g "F")
+  (add-edge! g "A" "B")
+  (add-edge! g "A" "C")
+  (add-edge! g "B" "D")
+  (add-edge! g "C" "D")
+  (add-edge! g "E" "F")
+  (with-open-file (stream #p"deps.dot" :direction :output :if-exists :supersede)
+    (format stream (simple-graph:to-dot (subgraph g "A")))))
